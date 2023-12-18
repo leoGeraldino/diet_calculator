804fb1005463881ae5ef7160764fe11fb5bd7fb6
leoGeraldino <leo.nardo_geraldino@hotmail.com>
2023-12-17 21:32:39 -0300

    Otimização da função de cálculo de calorias diárias

--- juicer.js
+++ juicer.js
@@ -4,45 +4,20 @@ function calculaCalorias() {
     const idade = parseInt(document.getElementById("idade").value);
     const sexo = document.getElementById("sexo").value;
     let atividade = parseInt(document.getElementById("atividadeFisica").value);
-    let tmb = 0;
+    let multiplicadorAtividade = 0;
 
-    if (sexo === "feminino"){
-        switch (atividade){
-            case 1:
-                tmb = 1.2 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
-                return tmb; 
-            case 2:
-                tmb = 1.375 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
-                return tmb; 
-            case 3:
-                tmb = 1.55 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
-                return tmb; 
-            case 4:
-                tmb = 1.725 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
-                return tmb; 
-            case 5: 
-                tmb = 1.9 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
-                return tmb;
-        }
-    }   else {
-        switch(atividade){
-            case 1: 
-                tmb = 1.2 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
-                return tmb;
-            case 2: 
-                tmb = 1.375 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
-                return tmb;
-            case 3: 
-                tmb = 1.55 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
-                return tmb;
-            case 4: 
-                tmb = 1.725 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
-                return tmb;
-            case 5: 
-                tmb = 1.9 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
-                return tmb;
-        }
+    switch (atividade) {
+        case 1: multiplicadorAtividade = 1.2; break;
+        case 2: multiplicadorAtividade = 1.375; break;
+        case 3: multiplicadorAtividade = 1.55; break;
+        case 4: multiplicadorAtividade = 1.725; break;
+        case 5: multiplicadorAtividade = 1.9; break;
+        default: return 0; 
     }
+
+    const baseTmb = (sexo === "feminino") ? 655 : 66;
+    const tmb = multiplicadorAtividade * (baseTmb + (9.6 * peso + 1.8 * altura - (sexo === "feminino" ? 4.7 : 6.8) * idade));
+    return tmb;
 }
 
 function consumoDiario() {
@@ -50,4 +25,3 @@ function consumoDiario() {
     const retornoConsumo = document.getElementById("caloriasDia");
     retornoConsumo.innerHTML = tmb.toFixed(2); 
 }
-
