177a70ad5806c7455283573eaf9bf0c4045ed83f
leoGeraldino <leo.nardo_geraldino@hotmail.com>
2023-12-17 21:23:46 -0300

    Lógica de cálculo de necessidades diárias ok

--- juicer.js
+++ juicer.js
@@ -1,10 +1,53 @@
 function calculaCalorias() {
-    const peso = document.getElementById("peso").value;
-    const altura = document.getElementById("altura").value;
-    const idade = document.getElementById("idade").value;
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("altura").value);
+    const idade = parseInt(document.getElementById("idade").value);
     const sexo = document.getElementById("sexo").value;
-    const atividadeFisica = document.getElementById("atividadeFisica").value;
-    const objetivo = document.getElementById("objetivo").value;
+    let atividade = parseInt(document.getElementById("atividadeFisica").value);
+    let tmb = 0;
 
-    const mapaSexo = 
+    if (sexo === "feminino"){
+        switch (atividade){
+            case 1:
+                tmb = 1.2 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
+                return tmb; 
+            case 2:
+                tmb = 1.375 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
+                return tmb; 
+            case 3:
+                tmb = 1.55 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
+                return tmb; 
+            case 4:
+                tmb = 1.725 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
+                return tmb; 
+            case 5: 
+                tmb = 1.9 * (655 + (9.6 * peso + 1.8 * altura - 4.7 * idade));
+                return tmb;
+        }
+    }   else {
+        switch(atividade){
+            case 1: 
+                tmb = 1.2 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
+                return tmb;
+            case 2: 
+                tmb = 1.375 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
+                return tmb;
+            case 3: 
+                tmb = 1.55 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
+                return tmb;
+            case 4: 
+                tmb = 1.725 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
+                return tmb;
+            case 5: 
+                tmb = 1.9 * (66 + (13.7 * peso + 5 * altura - 6.8 * idade));
+                return tmb;
+        }
+    }
 }
+
+function consumoDiario() {
+    const tmb = calculaCalorias();
+    const retornoConsumo = document.getElementById("caloriasDia");
+    retornoConsumo.innerHTML = tmb.toFixed(2); 
+}
+
